12eeaa415f1a73a3990dfd9a8cc2c7647fc7bb67
Ameer <ameerimtiaz4@gmail.com>
2022-12-02 10:56:13 +0500

    minor changes

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+ // projectId: 'hyb4kp',
+  projectId: '15rwu3',
+ watchForFileChanges: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
