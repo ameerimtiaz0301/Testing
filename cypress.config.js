68b907870462af6d49ad826ad66b559612a4acea
Ameer <ameerimtiaz4@gmail.com>
2022-12-03 17:10:20 +0500

    Component testing dependencies were added

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
 module.exports = {
- // projectId: 'hyb4kp',
-  projectId: '15rwu3',
- watchForFileChanges: false,
+  // projectId: 'hyb4kp',
+  projectId: "15rwu3",
+
+  watchForFileChanges: false,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "vue-cli",
+      bundler: "webpack",
     },
   },
 };
