340f951d7e25e2988c6b9a32faa1612f81374813
Salman <salmanrafiq226@gmail.com>
2022-12-05 18:14:56 +0500

    salman changes for user

--- cypress/e2e/Quick-eats-project/users.cy.js
+++ cypress/e2e/Quick-eats-project/users.cy.js
@@ -5,8 +5,14 @@ describe('Users view functions', function(){
         cy.get('input[type="email"]').type('khanmuhammadmalik@gmail.com').get('[type="password"]').type('123456')
         cy.get('[type="checkbox"]').check()
         cy.get('.btn').contains('Sign in').should('be.visible').click()
-    })
-    it('users', function(){
-        cy.xpath('//*[@id="app"]/div/div/div[1]/nav/ul/li[2]/a').scrollIntoView().should('be.visible').click()
-    })
+ })
+//     it('users', function(){
+//         cy.xpath('//*[@id="app"]/div/div/div[1]/nav/ul/li[2]/a').scrollIntoView().should('be.visible').click()
+//     })
+// })
+it('users', function(){
+    cy.xpath('//*[@id="app"]/div/div/div[1]/nav/ul/li[2]/a').scrollIntoView().should('be.visible').click()
+    cy.get('input[placeholder="Search with Name,Email,Phone,Bussiness name"]').type('newcutomer@gmail.com',{force: true})
+    cy.get('[class="btn btn-primary"]').contains('Search').dblclick({force: true})
+})
 })
