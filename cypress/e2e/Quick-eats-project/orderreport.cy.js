4812d6ff5414e9714c6dbf433f7eeef46bea963f
salmanrafiq226 <salmanrafiq226@gmail.com>
2023-01-10 13:27:53 +0500

    orderreoprt

--- cypress/e2e/Quick-eats-project/orderreport.cy.js
+++ cypress/e2e/Quick-eats-project/orderreport.cy.js
@@ -1,14 +1,19 @@
+ import { LoginPage } from "../../Pages/Loginpage.cy"
+ const loginPage = new LoginPage()
+ it('Log in', function () {
+     loginPage.navigate('https://deliverydev.quickeats.co.uk/login')
+     loginPage.enteremail()
+     loginPage.enterpassword()
+     loginPage.checkbox()
+     loginPage.clicksubmitbutton()
 
-//const { contains } = require("cypress/types/jquery")
+// describe('Order Customer Report', function(){
 
-describe('Order Customer Report', function(){
-
-
-it('User', function(){
-    cy.visit('https://deliverydev.quickeats.co.uk/login')
-    cy.get('input[type="email"]').type('khanmuhammadmalik@gmail.com').get('[type="password"]').type('123456')
-    cy.get('[type="checkbox"]').check()
-    cy.get('.btn').contains('Sign in').should('be.visible').click().wait(1000)
+// it('User', function(){
+//     cy.visit('https://deliverydev.quickeats.co.uk/login')
+//     cy.get('input[type="email"]').type('khanmuhammadmalik@gmail.com').get('[type="password"]').type('123456')
+//     cy.get('[type="checkbox"]').check()
+//     cy.get('.btn').contains('Sign in').should('be.visible').click().wait(1000)
 
     cy.get('.main_side > :nth-child(4) > .nav-link').contains('Orders Customer Report').click()
     cy.get('.row > :nth-child(1) > .form-control').select(6)
@@ -16,8 +21,7 @@ it('User', function(){
     //cy.get('[class="v3dp__heading"]').contains('January 2023').click({force:true})
     //cy.get('[class="v3dp__heading__center"]').contains('2023').click()
    // cy.get('[class="selected"]').contains('2022').dblclick()
-   cy.get('[class="v3dp__heading"]').contains('January 2023').invoke('val').then((text) => {
-    expect('09/01/2023').to.equal('09/01/2022');
+   cy.get('[class="v3dp__heading"]').contains('January 2023').find('input').eq(0)
+   .invoke('val').should('eq', '2020-05-01')
 })
-})
-})
+
