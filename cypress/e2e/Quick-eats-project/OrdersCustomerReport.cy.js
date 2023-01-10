65e509a29cded8cde997aa4cb9bc07a0e3ff2c37
ameer <ameerimtiaz4@gmail.com>
2023-01-10 19:19:06 +0500

    date picker file

--- cypress/e2e/Quick-eats-project/OrdersCustomerReport.cy.js
+++ cypress/e2e/Quick-eats-project/OrdersCustomerReport.cy.js
@@ -12,5 +12,13 @@ describe('Order customer report', function(){
     it('Order customer report', function(){
         cy.get('.main_side > :nth-child(4) > .nav-link').click()
         cy.get(':nth-child(2) > .v3dp__datepicker > .v3dp__input_wrapper > .form-control').should('be.visible').click()
+        cy.contains("January 2023").xpath('//*[@id="app"]/div/div/div[2]/main/div/div/div[3]/div/div[2]/div/div[4]/div[1]/button[2]').click()
+        cy.contains('2022').xpath('//*[@id="app"]/div/div/div[2]/main/div/div/div[3]/div/div[2]/div/div[3]/div[1]/button[2]').click()
+        cy.xpath('//*[@id="app"]/div/div/div[2]/main/div/div/div[3]/div/div[2]/div/div[2]/div[2]/div').contains('2021').click()
+        cy.contains('Jul').click()
+        cy.xpath('//*[@id="app"]/div/div/div[2]/main/div/div/div[3]/div/div[2]/div/div[4]/div[2]/div[2]/button[26]').contains('23').click()
     })
+//     it('select user name', function(){
+//         cy.get(':nth-child(3) > .row > :nth-child(1)').get('.row > :nth-child(1) > .form-control').select()
+//      })
 })
